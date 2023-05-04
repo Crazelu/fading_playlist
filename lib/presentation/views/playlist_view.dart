ab36beee3f16ec3642f6884972886387418a0ed1
Crazelu <eberelucky64@gmail.com>
2023-05-04 18:48:00 +0100

    fix: adds Android internet permission and updates loading UI

--- lib/presentation/views/playlist_view.dart
+++ lib/presentation/views/playlist_view.dart
@@ -1,6 +1,7 @@
 import 'package:fading_playlist/core/playlist_controller.dart';
 import 'package:fading_playlist/models/downloaded_track.dart';
 import 'package:fading_playlist/models/track.dart';
+import 'package:fading_playlist/presentation/widgets/custom_text.dart';
 import 'package:fading_playlist/presentation/widgets/now_playing_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
@@ -48,8 +49,19 @@ class _PlaylistViewState extends State<PlaylistView> {
             builder: (_, tracks, __) {
               if (tracks.isEmpty) {
                 return Center(
-                  child: Lottie.asset("assets/vinyl-loading.json"),
-                  //source: https://lottiefiles.com/88944-vinyl-loading
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //source: https://lottiefiles.com/88944-vinyl-loading
+                      Lottie.asset("assets/vinyl-loading.json"),
+                      const SizedBox(height: 16),
+                      const CustomText(
+                        text: "Getting your playlist ready...",
+                        fontSize: 16,
+                      ),
+                      const SizedBox(height: 48),
+                    ],
+                  ),
                 );
               }
 
